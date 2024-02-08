c623f36e47b45f6fe40c5733c6a82419e52b99ef
katgur <mitsutanaka29@gmail.com>
2024-02-08 19:44:58 +0300

    Скролл

--- src/component/ui/Scroll/index.jsx
+++ src/component/ui/Scroll/index.jsx
@@ -1,11 +1,13 @@
+import './Scroll.css';
+
 function Scroll({ total, current }) {
 
     var marginLeft = Math.round(current / total * 100);
     var width = Math.round((1 / total * 100));
 
     return (
-        <div className="scroll-track">
-            <div className="scroll-thumb" style={{ marginLeft: `${marginLeft}%`, width: `${width}%` }}></div>
+        <div className="scroll">
+            <div className="scroll__track" style={{ marginLeft: `${marginLeft}%`, width: `${width}%` }}></div>
         </div>
     )
 }
