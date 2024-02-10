fe890fa39c9c46f62deff9706c4dbbe8de1dc482
katgur <mitsutanaka29@gmail.com>
2024-02-10 23:21:58 +0300

    Добавлен wrapper для уведомлений

--- src/feature/notification/Notifications.jsx
+++ src/feature/notification/Notifications.jsx
@@ -3,14 +3,30 @@ import { getNotifications } from "./notificationSlice"
 import Alert from '../../component/ui/Alert'
 import { removeNotification } from "./notificationSlice";
 import { createPortal } from "react-dom";
+import { useEffect, useState } from "react";
 
 function Notifications() {
     const notifications = useSelector(getNotifications)
     const dispatch = useDispatch();
+    const [wrapper, setWrapper] = useState(null);
 
-    const wrapper = document.getElementById("alert");
+    useEffect(() => {
+        let newWrapper = document.getElementById('alert');
+        if (!newWrapper) {
+            newWrapper = document.createElement('div');
+            newWrapper.setAttribute('id', 'alert');
+            document.body.appendChild(newWrapper);
+        }
+        setWrapper(newWrapper);
 
-    return createPortal(notifications.length && <ul>
+        return () => {
+            if (newWrapper.parentNode) {
+                newWrapper.parentNode.removeChild(newWrapper);
+            }
+        }
+    }, [])
+
+    return wrapper && createPortal(notifications.length !== 0 && <ul>
         {
             notifications.map(notification => {
                 return <Alert key={notification.id} {...notification} onClose={() => dispatch(removeNotification(notification.id))} />
